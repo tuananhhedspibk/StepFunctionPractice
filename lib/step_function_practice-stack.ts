0a55116483eba3dbc0fa71019299e4ba0c3677ac
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-10-25 08:01:09 +0900

    :boom: CDK with stepfunction

--- lib/step_function_practice-stack.ts
+++ lib/step_function_practice-stack.ts
@@ -1,16 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as fs from 'fs';
 
-export class StepFunctionPracticeStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+class JobPollerStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
 
-    // The code that defines your stack goes here
+    const getStatusLambda = new lambda.Function(this, 'CheckLambda', {
+      code: new lambda.InlineCode(fs.readFileSync('lambdas/check_status.py', { encoding: 'utf-8' })),
+      handler: 'index.main',
+      timeout: cdk.Duration.seconds(30),
+      runtime: lambda.Runtime.PYTHON_3_9
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StepFunctionPracticeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const submitLambda = new lambda.Function(this, 'SubmitLambda', {
+      code: new lambda.InlineCode(fs.readFileSync('lambdas/submit.py', { encoding: 'utf-8' })),
+      handler: 'index.main',
+      timeout: cdk.Duration.seconds(30),
+      runtime: lambda.Runtime.PYTHON_3_9
+    });
+
+    const submitJob = new tasks.LambdaInvoke(this, 'Submit Job', {
+      lambdaFunction: submitLambda,
+      outputPath: '$.Payload'
+    });
+    const waitX = new sfn.Wait(this, 'Wait X Seconds', {
+      time: sfn.WaitTime.duration(cdk.Duration.seconds(30))
+    });
+    const getStatus = new tasks.LambdaInvoke(this, 'Get Job Status', {
+      lambdaFunction: getStatusLambda,
+      outputPath: '$.Payload'
+    });
+    const jobFailed = new sfn.Fail(this, 'Job Failed', {
+      cause: 'AWS Batch Job Failed',
+      error: 'DescribeJob returned FAILED'
+    });
+    const finalStatus = new tasks.LambdaInvoke(this, 'Get Final Job Status', {
+      lambdaFunction: getStatusLambda,
+      outputPath: '$.Payload'
+    });
+
+    const definition = submitJob.next(waitX)
+      .next(getStatus)
+      .next(new sfn.Choice(this, 'Job Complete ?')
+        .when(sfn.Condition.stringEquals('$.status', 'FAILED'), jobFailed)
+        .when(sfn.Condition.stringEquals('$.status', 'SUCCEEDED'), finalStatus)
+        .otherwise(waitX)
+      );
+
+    const stateMachine = new sfn.StateMachine(this, 'CronStateMachine', {
+      definition,
+      timeout: cdk.Duration.minutes(5)
+    });
+
+    submitLambda.grantInvoke(stateMachine.role);
+    getStatusLambda.grantInvoke(stateMachine.role);
+
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression('cron(0 18 ? * MON-FRI *)')
+    });
+    rule.addTarget(new targets.SfnStateMachine(stateMachine));
   }
 }
+
+const app = new cdk.App();
+new JobPollerStack(app, 'aws-stepfunctions-integ');
+app.synth();
