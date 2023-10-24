f7a29ec6bef635c0c97d7ad31c9a8942a4a5d2c7
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-10-24 23:25:07 +0900

    Initial commit

---
+++ test/step_function_practice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepFunctionPractice from '../lib/step_function_practice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step_function_practice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepFunctionPractice.StepFunctionPracticeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
